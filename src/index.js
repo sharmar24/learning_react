2e0245f17eb28cfecd07174070f8f61b77cab412
sharmar24 <88975095+sharmar24@users.noreply.github.com>
2022-08-30 16:18:43 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> Hello World! </h1>
+    <ul>
+      <li>My Name is Rajat</li>
+      <li>I am learning React.</li>
+      <li>I am super excited about it.</li>
+    </ul>
+  </div>,
+  document.querySelector("#app")
+);
